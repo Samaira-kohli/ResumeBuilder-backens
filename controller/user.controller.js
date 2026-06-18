04f96003dc2e1a51638ed11aa002c4ca6fa97751
Samaira-kohli <san.magicwand@gmail.com>
2026-06-18 17:37:00 +0530

    Fix cross-site authentication cookies

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -30,8 +30,8 @@ export const registerUser = async (req, res) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: false,
-            sameSite: 'strict',
+            secure: true,
+            sameSite: 'none',
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
@@ -72,8 +72,8 @@ export const loginUser = async (req, res) => {
 
             res.cookie('token', token, {
                 httpOnly: true,
-                secure: false,
-                sameSite: 'strict',
+                secure: true,
+                sameSite: 'none',
                 maxAge: 7 * 24 * 60 * 60 * 1000
             })
 
